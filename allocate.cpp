18684aefb637a08f6bd8cf154287f26875faf41d
Globus <factsleet@gmail.com>
2026-01-20 18:09:37 +0300

    Update allocate.cpp

--- allocate.cpp
+++ allocate.cpp
@@ -1,19 +1,19 @@
 char __fastcall allocate_140068BD4(__int64 process, __int64 allocate_structure)
 {
-  char v2; // bp
+  char result; // bp
   int m_protect_check; // eax
   int m_protect; // ebx
   int m_allocation_type; // edi
   __int64 m_zero_bits; // rsi
-  unsigned __int64 v9; // rax
+  unsigned __int64 decrypted_ptr_result; // rax
   __int64 (__fastcall **allocate_virtual_memory)(__int64, __int64 *, __int64, __int64 *, int, int); // rax
   int allocate_result; // ebx
   _BYTE v13[16]; // [rsp+30h] [rbp-68h] BYREF
-  _BYTE v14[88]; // [rsp+40h] [rbp-58h] BYREF
+  _BYTE state[88]; // [rsp+40h] [rbp-58h] BYREF
   __int64 m_address; // [rsp+A0h] [rbp+8h] BYREF
   __int64 m_region_size; // [rsp+B0h] [rbp+18h] BYREF
 
-  v2 = 0;
+  result = 0;
   if ( !process )
     return 0;
   if ( !allocate_structure )
@@ -25,15 +25,15 @@ char __fastcall allocate_140068BD4(__int64 process, __int64 allocate_structure)
   m_protect_check = *(_DWORD *)(allocate_structure + 36);
   if ( !m_protect_check || (m_protect_check & 0xF0) != 0 || *(_QWORD *)(allocate_structure + 8) )
     return 0;
-  if ( attach_process_14003CE18(process, (__int64)v14) )
+  if ( attach_process_14003CE18(process, (__int64)state) )
   {
     m_protect = *(_DWORD *)(allocate_structure + 36);
     m_allocation_type = *(_DWORD *)(allocate_structure + 32);
     m_zero_bits = *(_QWORD *)(allocate_structure + 16);
     m_address = *(_QWORD *)(allocate_structure + 8);
     m_region_size = *(_QWORD *)(allocate_structure + 24);
-    v9 = decrypt_ptr_14002D81C(qword_1402097B0);
-    allocate_virtual_memory = (__int64 (__fastcall **)(__int64, __int64 *, __int64, __int64 *, int, int))sub_14000F4B4((__int64)v13, __ROR8__(~v9, 17) ^ 0xAB1276D877943869uLL);
+    decrypted_ptr_result = decrypt_ptr_14002D81C(qword_1402097B0);
+    allocate_virtual_memory = (__int64 (__fastcall **)(__int64, __int64 *, __int64, __int64 *, int, int))sub_14000F4B4((__int64)v13, __ROR8__(~decrypted_ptr_result, 17) ^ 0xAB1276D877943869uLL);
     allocate_result = (*allocate_virtual_memory)(
                         -1,
                         &m_address,
@@ -44,11 +44,11 @@ char __fastcall allocate_140068BD4(__int64 process, __int64 allocate_structure)
     sub_14000F4A0((__int64)v13);
     if ( allocate_result >= 0 )
     {
-      v2 = 1;
+      result = 1;
       *(_QWORD *)(allocate_structure + 8) = m_address;
       *(_QWORD *)(allocate_structure + 24) = m_region_size;
     }
-    sub_14003D0AC(process, (__int64)v14);
+    sub_14003D0AC(process, (__int64)state);
   }
-  return v2;
+  return result;
 }
